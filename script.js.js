10c79913063e0c8581bc60b93ad6c6ac138d593f
Balogun Olatunde Ayoade <105291683+Tunneskey@users.noreply.github.com>
2022-06-28 00:46:19 +0100

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,18 @@
+var opr= prompt('Please Enter operator; e.g +,-,*,/');
+var num1= parseInt(prompt('number 1: '));
+var num2= parseInt(prompt('number 2: '));
+
+if(opr == "+")
+{
+    alert(num1 + num2)
+}else if(opr == "-")
+{
+    alert(num1 - num2)  
+}else if(opr == "*")
+{
+    alert(num1 * num2)  
+}else if(opr == "/")
+{
+    alert(num1 / num2)
+}
+
